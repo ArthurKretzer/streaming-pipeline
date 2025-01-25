4b3f77221d2d280a83c95189a713e394bcf3193b
Arthur Kretzer <arthur.raulino.kretzer@gmail.com>
2025-01-25 16:08:51 -0300

    fix: load env import order and produce input argument

--- src/main.py
+++ src/main.py
@@ -3,22 +3,22 @@ import time
 
 from dotenv import load_dotenv
 
-from src.common.logger import log
-from src.data_process import DataProcess
-from src.kafka_consumer import KafkaConsumer
-from src.kafka_producer import KafkaProducer
-
 # Load environment variables from .env file
-load_dotenv("./.env")
+load_dotenv()
+
+from common.logger import log
+from data_process import DataProcess
+from kafka_consumer import KafkaConsumer
+from kafka_producer import KafkaProducer
 
 logger = log("Main")
 
 kafka_broker = "kafka-cpc.certi.org.br:31289"  # Kafka Bootstrap server
 
 
-def produce(topic_name: str = "robot"):
+def produce(topic_name: str = "robot", data_type: str = "mocked"):
     producer = KafkaProducer(kafka_broker, topic_name)
-    producer.produce()
+    producer.produce(data_type)
 
 
 def consume(topic_name: str = "robot"):
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     if function_name == "consume":
         consume(topic_name)
     elif function_name == "produce":
-        produce(topic_name)
+        produce(topic_name, "mocked")
     elif function_name == "process":
         process()
     else:
